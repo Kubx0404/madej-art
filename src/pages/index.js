520c078970d8bf419f4a3c372dc8776b58729694
kubx0404 <56558546+Kubx0404@users.noreply.github.com>
2023-01-20 09:43:59 +0100

    animation added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,15 @@
+// import '@/styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+
 export default function home() {
   return (
     <>
-      <h1>Work in progress . . . </h1>
+      <div className={styles.firstdiv}>
+        <h1 className={styles.inline}>Work in progress </h1> &nbsp;
+        <span className={styles.dot}> . </span>
+        <span className={styles.dot2}> . </span>
+        <span className={styles.dot3}> . </span>
+      </div>
     </>
-  )
+  );
 }
