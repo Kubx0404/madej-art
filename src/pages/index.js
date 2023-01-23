42cd6e18ab26fba23d721ace362b36a71414933b
kubx0404 <56558546+Kubx0404@users.noreply.github.com>
2023-01-23 23:47:40 +0100

    better smaller window compatibility added

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,34 +13,53 @@ export default function home() {
         <div className="media_div"></div>
         <div className="media_div"></div>
         <div className="media_div"></div>
-        <div className="media_div"></div>
+        <div className="media_div" style={{ marginRight: "0px" }}></div>
       </div>
 
-      <div className="big_div bio_div" style={{ height: "50vh" }}>
-        <p
-          className="name"
-          style={{ marginBottom: "0px", marginBlockStart: "1em" }}
-        >
-          Jakub Madejczyk
-        </p>
-        <p style={{ marginLeft: "7em", marginTop: "0px" }} className="bio_text">
-          Still developing fungus.
-        </p>
-        <p className="bio_text" style={{ width: "40vw", marginLeft: "5em" }}>
-          Hello! I’m an student of an IT high school in Poznan. My future goal
-          is to learn more and more in case of coding and IT stuff. One of my
-          biggest interests is photography and teaching rollerblading.
-        </p>
+      <div
+        className="big_div bio_div"
+        style={{ minHeight: "50vh", justifyContent: "space-evenly" }}
+      >
+        <div>
+          <p
+            className="name"
+            style={{ marginBottom: "0px", marginBlockStart: "1em" }}
+          >
+            Jakub Madejczyk
+          </p>
+          <p
+            style={{ marginLeft: "10vw", marginTop: "0px", width: "26vw" }}
+            className="bio_text"
+          >
+            Still developing fungus.
+          </p>
+          <p className="bio_text" style={{ width: "40vw", marginLeft: "6vw" }}>
+            Hello! I’m an student of an IT high school in Poznan. My future goal
+            is to learn more and more in case of coding and IT stuff. One of my
+            biggest interests is photography and teaching rollerblading.
+          </p>
+        </div>
+
         <img
           src="photo.png"
           alt="photo"
           style={{
             borderRadius: "50%",
-            height: "260px",
-            width: "260px",
+            // height: "260px",
+            // width: "260px",
+            // height: "28vh",
+            // width: "13vw",
+            width: "25vmin",
             border: "6px solid #494B50",
-            marginTop: "2.8em",
-            marginRight: "5em",
+            // marginTop: "2.8em",
+            marginTop: "6vmin",
+            marginRight: "4vw",
+            marginBottom: "4vw",
+
+            // marginRight: "5vw",
+            // aspectRatio: "1/1",
+            alignSelf: "flex-start",
+            minWidth: "0",
           }}
         />
       </div>
@@ -48,8 +67,8 @@ export default function home() {
       <div
         className="big_div"
         style={{
-          paddingLeft: "2.5em",
-          paddingRight: "2.5em",
+          paddingLeft: "5vmin",
+          paddingRight: "5vmin",
           justifyContent: "space-between",
           // padding: "1.5em",
           // paddingTop: "1.5em",
@@ -61,9 +80,11 @@ export default function home() {
             src="code.svg"
             alt="code"
             style={{
-              height: "90px",
-              width: "90px",
+              // height: "90px",
+              // width: "90px",
+              height: "4.5vw",
             }}
+            x
           />
         </div>
         <div className="big_button">
@@ -71,8 +92,9 @@ export default function home() {
             src="camera.svg"
             alt="code"
             style={{
-              height: "90px",
-              width: "90px",
+              height: "4.5vw",
+              // width: "3.5vw",
+              // aspectRatio: "1/1",
             }}
           />
         </div>
@@ -81,8 +103,7 @@ export default function home() {
             src="student.svg"
             alt="code"
             style={{
-              height: "90px",
-              width: "90px",
+              height: "4.5vw",
             }}
           />
         </div>
@@ -91,8 +112,7 @@ export default function home() {
             src="rollerblade.svg"
             alt="code"
             style={{
-              height: "90px",
-              width: "90px",
+              height: "4.5vw",
             }}
           />
         </div>
